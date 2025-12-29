1fdc49c5a3d04fbc0e2616d02482b187a17116d7
Anootosh Sarkar <kage.yukami45anootosh@gmail.com>
2025-12-29 05:08:19 +0000

    Updates as on 29-Dec-2025

--- Sandbox_1/Basic_GUI.py
+++ Sandbox_1/Basic_GUI.py
@@ -1,3 +1,15 @@
+#Excercise_1: 
+
+User_Name = input('What is your name? ')
+Birth_Year = input('What year were you born in? ')
+Current_Year = input('What is the current year? ') 
+Age = int(Current_Year) - int(Birth_Year)
+
+print(f'Hey {User_Name}, you\'re {Age} years old in {Current_Year}. \n Welcome to Python! ')
+
+
+#Excercise_2: 
+
 print('Hello')
 #Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to 
 # be '*'. This will reveal an image!
