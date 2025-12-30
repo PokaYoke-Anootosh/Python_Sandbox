74cb2f6b1fd4470e8559be25423bc977a3067b9e
Anootosh Sarkar <kage.yukami45anootosh@gmail.com>
2025-12-30 05:20:09 +0000

    Updates as on 30-Dec-2025

---
+++ Sandbox_1/User_Input.py
@@ -0,0 +1,6 @@
+User_Name = input('What is your name? ')
+Birth_Year = input('What year were you born in? ')
+Current_Year = input('What is the current year? ') 
+Age = int(Current_Year) - int(Birth_Year)
+
+print(f'Hey {User_Name}, you\'re {Age} years old in {Current_Year}. \n Welcome to Python! ')
